27a596f87e140c2b65c83afb72f93d41a6d86377
JARRIPOTHULARAKESH <jrakeshchintu@gmail.com>
2025-04-29 15:59:40 +0530

    added progrm to day2

--- day2.py
+++ day2.py
@@ -42,7 +42,7 @@ obj=abc()
 val=int(input("enter the number:"))
 obj.eo(val)'''
 #segregrate the even and odd parameters in a list and print even list and odd list seperately using a class "number"
-class number:
+'''class number:
     odd=[]
     even=[]
     def __init__(self,num):
@@ -57,5 +57,27 @@ n3=number(43)
 n4=number(54)
 n5=number(65)
 print(number.even)
-print(number.odd)
-
+print(number.odd)'''
+#class methods
+class abc:
+    def __init__(self,name,var):
+        self.name=name
+        self.var=var
+    def __repr__(self):
+        return repr(self.var)
+    def __len__(self):
+        return len(self.name)
+    def __cmp__(self,obj):
+        return self.var-obj.var
+obj=abc("abcdef",10)
+print("the value stored in object is:",repr(obj))
+print("the lenght of name stored in object:",len(obj))
+obj1=abc("ghjijl",1)
+val=obj.__cmp__(obj1)
+if val==0:
+    print("both vallues ar equal")
+elif val==-1:
+    print("1st value is less than second")
+else:
+    print("2nd value is less than first")
+    
