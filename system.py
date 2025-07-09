3afbd4569d894754ef38f66a3e3b1c9b8d33ac26
JARRIPOTHULA RAKESH <jrakeshchintu@gmail.com>
2025-07-09 15:33:34 +0530

    first commit

--- system.py
+++ system.py
@@ -1,5 +1,5 @@
-import sys
-'''print("Script name:",sys.argv[0])
+'''import sys
+print("Script name:",sys.argv[0])
 print("All args:",sys.argv[1:])
 print("Number of items:",len(sys.argv))
 print("Included file name:",sys.argv)
@@ -13,10 +13,19 @@ num2=int(sys.argv[2])
 num3=int(sys.argv[3])
 print("product:",num1*num2*num3)'''
 
-if len(sys.argv) != 3:
-    print("Usage:python sample.py l b")
-else:
-    l = int(sys.argv[1])
-    b = int(sys.argv[2])
-    area = l * b
-    print("Area of rectangle:", area)
+import argparse
+parser=argparse.ArgumentParser(description="Simple Calculator.")
+parser.add_argument("--x", type=int,required=True, help="First number")
+
+parser.add_argument("--y", type=int,required=True, help="Second number")
+parser.add_argument("--opt", type=str,required=True, choices=["add","sub","mul","div"], help="Operation to perform")
+args=parser.parse_args()
+if args.opt=="add":
+    result=args.x+args.y
+elif args.opt=="sub":
+    result=args.x-args.y
+elif args.opt=="mul":
+    result=args.x*args.y
+elif args.opt=="div":
+    result=args.x/args.y
+print("Result is:",result)
