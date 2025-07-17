e1008c836fbe7c98b1ad4d95bde187c2f4629eef
JARRIPOTHULA RAKESH <jrakeshchintu@gmail.com>
2025-07-17 13:01:31 +0530

    Add existing project files to Git

--- new_folder/own.py
+++ new_folder/own.py
@@ -1,2 +1,17 @@
-colors=['red', 'green', 'blue','yellow', 'orange']
-print([color.upper() for color in colors ])
+'''colors=['red', 'green', 'blue','yellow', 'orange']
+print([color.upper() for color in colors ])'''
+
+from calendar import *
+year =int(input("Enter a year: "))
+if isleap(year):
+    print(year,"is leap year")
+else:
+    print(year,"year is not leap year")
+
+'''from datetime import *
+d=date.today()
+print(d)
+d=date(1966,6,29)
+for x in range(1,10):
+    nextdate=d+timedelta(days=x)
+    print(nextdate)'''
