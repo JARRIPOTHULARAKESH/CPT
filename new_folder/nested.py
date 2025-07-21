fb95ef436679a47f59e6bed11dcb4d8127629d46
JARRIPOTHULA RAKESH <jrakeshchintu@gmail.com>
2025-07-21 11:00:32 +0530

    Add existing project files to Git

--- new_folder/nested.py
+++ new_folder/nested.py
@@ -9,7 +9,7 @@
 o=(lambda a: lambda b,c:(a+b)*c)(5)(3,2)
 print(o)  # This will print the result of the lambda expression)
 
-o=(lambda a: lambda b: lambda c: lambda d:(a+b)*c*d)(2)(3)(4)(5)
+'''o=(lambda a: lambda b: lambda c: lambda d:(a+b)*c*d)(2)(3)(4)(5)
 print(o)  # This will print the result of the nested lambda expression
 
 
@@ -17,4 +17,12 @@ num=int(input("a:"))
 n=int(input("x:"))
 oper=lambda a:lambda x:(x+a)**2
 numsq=oper(num) #5
-print(numsq(n))  # This will print the square of (n + num)
+print(numsq(n))  # This will print the square of (n + num)'''
+
+try:
+    o=(lambda a: lambda b: lambda c,d:(a-b)/(c-d)(10)(4)(8,8))
+    print(o)
+except ZeroDivisionError:
+    print("Division by zero is not allowed.")
+except typeError:
+    print("Invalid input type. Please ensure all inputs are numbers.")
