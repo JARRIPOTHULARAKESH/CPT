7d2aebc9ba7d2eef8624c04e0830ba21e0692d74
JARRIPOTHULA RAKESH <jrakeshchintu@gmail.com>
2025-07-21 10:18:14 +0530

    Add existing project files to Git

---
+++ new_folder/nested.py
@@ -0,0 +1,13 @@
+#lambda functions
+'''(a+b)*c expression lambda evaluate and returns to another lambda'''
+
+#l2=lambda a: lambda b,c:(a+b)*c # This lambda takes one argument and returns another lambda
+#l1=l2(3)
+#value=l1(1,2)
+# Output the result
+#print(value)
+o=(lambda a: lambda b,c:(a+b)*c)(5)(3,2)
+print(o)  # This will print the result of the lambda expression)
+
+o=(lambda a: lambda b: lambda c: lambda d:(a+b)*c*d)(2)(3)(4)(5)
+print(o)  # This will print the result of the nested lambda expression
