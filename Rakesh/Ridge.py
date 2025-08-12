69f685a2f14a2fe9e1501a4e41e14019c538c980
JARRIPOTHULA RAKESH <jrakeshchintu@gmail.com>
2025-08-12 11:47:04 +0530

    Add existing project files to Git

--- Rakesh/Ridge.py
+++ Rakesh/Ridge.py
@@ -1,24 +1,11 @@
-# from sklearn.linear_model import Lasso
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import mean_squared_error
-# from sklearn.datasets import make_regression
-# import numpy as np
-
-# np.random.seed(42)
-# x,y=make_regression(n_samples=100, n_features=10, noise=10.0,random_state=42)
-# x_train,y_train,x_test,y_test= train_test_split(x,y,test_size=0.2, random_state=42)
-# lasso= Lasso(alpha=1.0)
-# lasso.fit(x_train, y_train)
-# y_pred= lasso.predict(x_test)
-# print("Lasso Regression Coefficients:", lasso.coef_)
-# print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.datasets import make_regression
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
 
 np.random.seed(42)
 
@@ -28,14 +15,13 @@ X, y = make_regression(n_samples=100, n_features=10, noise=15, random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+ridge= Ridge(alpha=1.0)
+ridge.fit(X_train, y_train)
 
-lasso = Lasso(alpha=1.0)
-lasso.fit(X_train, y_train)
 
+y_pred = ridge.predict(X_test)
 
-y_pred = lasso.predict(X_test)
-
-print("Lasso Regression Coefficients:", lasso.coef_)
+print("ridge Regression Coefficients:", ridge.coef_)
 print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
 
 
@@ -62,7 +48,7 @@ for i in range(2, X.shape[1]):
     x_grid[:, i] = np.mean(X_test[:, i])
 
 
-y_grid = lasso.predict(x_grid).reshape(x1_grid.shape)
+y_grid = ridge.predict(x_grid).reshape(x1_grid.shape)
 
 
 ax.plot_surface(x1_grid, x2_grid, y_grid, color='yellow', alpha=0.5)
@@ -70,6 +56,10 @@ ax.plot_surface(x1_grid, x2_grid, y_grid, color='yellow', alpha=0.5)
 ax.set_xlabel('Feature 1')
 ax.set_ylabel('Feature 2')
 ax.set_zlabel('Target Variable')
-ax.set_title('Lasso Regression 3D Visualization')
+ax.set_title('Ridge Regression 3D Visualization')
 plt.legend()
+
+def rotate(angle):
+    ax.view_init(azim=angle)
+ani = FuncAnimation(fig, rotate, frames=np.arange(0, 360, 2), interval=50)
 plt.show()
